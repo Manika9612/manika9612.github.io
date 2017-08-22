2e9950bec62c6cfc5ee34d4c1cccf9b53186de4e
Manika9612 <31210848+Manika9612@users.noreply.github.com>
2017-08-22 22:06:44 +0530

    Add files via upload

---
+++ data/home.php
@@ -0,0 +1,329 @@
+
+
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Home</title>
+<link rel="stylesheet" href="../css/bootstrap.min.css" />
+<style>
+
+.nav-tabs {
+    border-bottom:none;
+	background-color:#000000;
+	color:#FF0000;
+	font-family:cursive;
+	font-size:24px;
+	position:fixed;
+
+	}
+	.nav-tabs>li>a {
+    margin-right: 2px;
+    line-height: 1.42857143;
+    border: 1px solid transparent;
+    border-radius: 4px 4px 0 0;
+    color: red;
+}
+.blue
+{
+background-color:#000033;
+thick:20px;
+}
+
+marquee
+{
+line-height:auto;
+font-family:Jokerman;
+font-size:24px;
+color:#0000CC;
+}
+h3
+{
+font-family:cursive;
+color:#FF0000;
+}
+
+table td
+{
+font-family:cursive;
+color:#003366;
+}
+.text-center {
+    font-family: cursive;
+    font-size: 20px;
+    text-decoration: blink;
+    color: red;
+}
+h4,p
+{
+color:#003366;
+font-family:cursive;
+}
+.align
+{
+color:#000066;
+font-family:cursive;
+font-size:18px;
+}
+.bgcolor
+{
+background-color:#000000;
+}
+</style>
+</head>
+
+<body>
+
+<div class="container-fluid">
+	<div class="row">
+    <div class="col-sm-6">
+    <marquee behavior="alternate" scrolldelay="200px" class="fixed">PORTFOLIO</marquee>
+    </div><!-- col-->
+    <div class="col-sm-6">
+            <ul class="nav nav-tabs" role="tablist">
+              <li
+              ><a href="#">About Me</a></li>
+              <li><a href="#">Education</a></li>
+              <li><a href="#">Current Status</a></li>
+              <li><a href="#">Skills</a></li>
+              <li><a href="#">Contact</a></li>
+            </ul>
+    </div><!-- col -->
+    </div><!-- row -->
+    <hr class="black"></hr>
+</div><!-- container -->
+<div class = "container-fluid">
+<div class="row">
+<div class="text-center">About Me</div>
+</div><!-- row -->
+	<div class="row">
+        <div class="col-sm-3">
+        <img src="../images/1test.jpg"></img>
+        </div><!-- col -->
+        <div class="col-sm-4">
+        <h3>Hy, I am Manika Puri!!!!!</h3>
+         <h4>"Designer & Developer"</h4>
+         <br>
+        <table width="600px" cellspacing="0" cellpadding="0">
+        <tr>
+        <td>Date Of Birth</td>
+        <td>12th May 1996</td>
+        </tr>
+        <tr>
+        <td>Phone</td>
+        <td>9018230544</td>
+        </tr>
+        <tr>
+        <td>Address</td>
+        <td>H.No.127, Malhotra Street, City Chownk,Jammu </td>
+        </tr>
+         <tr>
+        <td>E-mail</td>
+        <td>manika.puri35@gmail.com</td>
+        </tr>
+        </table>
+        <br>
+        <a href="../ManikaResume.pdf" class="btn btn-danger" download> You can Download my Resume..</a>
+	 </div><!-- col -->
+      <div class="col-sm-4">
+    <h3 class="text-center" ob> Objective</h3>
+    <p>
+    "To work for a company which will help me in value addition as i am passionate 
+to the level where I complete my objectives with quality.Most of all I want a 
+new challenge in my life to strengthen and improve my skills.Moreover,”I am 
+applying for a career and others are applying for job” 
+   </p>
+</div><!-- col -->
+</div><!-- row -->    
+</div><!-- container -->
+ <hr class="darkblue"></hr>
+<div class="container-fluid 1">
+<div class="row">
+	<div class="col-sm-8">
+    <h3 class="text-center">Education</h3>
+    <table width="750px" cellspacing="1" cellpadding="0" border="1px" height="200">
+        <tr>
+        <th>Degree/qualification</th>
+        <th>Name of the Institute/School</th>
+        <th>University/Board</th>
+        <th>Percentage</th>
+        <th>Month or Year of Passing</th>
+        </tr>
+         <tr>
+         <td>12th</td>
+         <td>GGHSS,Mubarakh Mandi,Jammu</td>
+         <td>JKBOSE</td>
+         <td>94%</td>
+         <td>2014</td>
+        </tr>
+        <tr>
+         <td>10th</td>
+         <td>Model Academy,Jammu</td>
+         <td>JKBOSE</td>
+         <td>93.4%</td>
+         <td>2012</td>
+        </tr>
+        
+        </table>
+        <br>
+        <table width="750px" cellspacing="1" cellpadding="2" border="1px" height="150">
+        <tr>
+        <th>1st Semester</th>
+        <th>2nd Semester</th>
+        <th>3rd Semester</th>
+        <th>4th Semester</th>
+        <th>5th Semester</th>
+        <th>Overall</th>
+        </tr>
+         <tr>
+        <td>73.90%</td>
+        <td>72.20%</td>
+        <td>77.80%</td>
+        <td>77.50%</td>
+        <td>77.40%</td>
+         <td>75.76%</td>
+        </tr>
+         
+        </table>
+    
+    
+    </div><!-- col -->
+    <div class="col-sm-4">
+     <h3>Current Status</h3>
+     <p>B.tech Computer Science Engineering</p>
+     <p>Model Institute Of Engineering & Technology</p>
+     <p>Jammu,Jammu & kashmir (J&K), INDIA</p>
+     
+    </div><!-- col -->
+    </div><!-- row -->
+ <hr class="blue"></hr>
+ <div class="container">
+ <div class="row">
+ <h3 class="text-center">Skills</h3>
+ <div class="row">
+ <div class="col-sm-6">
+ <h3 class="text-center">Technical Skills</h3>
+ <div class="row">
+ <div class="progress">
+  <div class="progress-bar" role="progressbar" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100" style="width: 60%;">
+   HTML5 60% 
+  </div>
+  </div>
+  </div><!-- row -->
+  <div class="row">
+ <div class="progress">
+  <div class="progress-bar" role="progressbar" aria-valuenow="40" aria-valuemin="0" aria-valuemax="100" style="width: 40%;">
+   CSS3 40%
+  </div>
+  </div>
+  </div><!-- row -->
+  <div class="row">
+ <div class="progress">
+  <div class="progress-bar" role="progressbar" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100" style="width: 10%;">
+   JS 10%
+  </div>
+  </div>
+  </div><!-- row -->
+  <div class="row">
+ <div class="progress">
+  <div class="progress-bar" role="progressbar" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100" style="width: 20%;">
+   PHP 20%
+  </div>
+  </div>
+  </div><!-- row -->
+  <div class="row">
+ <div class="progress">
+  <div class="progress-bar" role="progressbar" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100" style="width: 30%;">
+   My SQL 30%
+  </div>
+  </div>
+  </div><!-- row -->
+</div><!-- col -->
+ <div class="col-sm-6">
+ <h3 class="text-center">Extra Skills</h3>
+  <div class="row align">
+  <div class="col-sm-2">
+  </div>
+  <div class="col-sm-4">
+  <ul>
+  <li>Badminton
+  <li>Music
+  <li>Travelling
+  <li>Cricket
+  <li>Internet Surfing
+  </ul>
+  </div>
+  </div>
+ </div><!-- col -->
+ </div><!-- row -->
+ </div><!-- row -->
+ </div><!-- container -->
+ <hr class="blue"></hr>
+ <div class="row">
+ <h3 class="text-center">Contact Us</h3>
+ <div class="col-sm-6">
+ <div class ="wrap">
+	<div class="container" id="header-container">
+    	
+         <div class="row">
+         	
+           
+            <div class="col-sm-4">
+             <input type="text" class="form-control" id="name" name="name" placeholder="Name" />
+            </div><!-- col -->
+          </div><!--row--> 
+          
+          <div class="row">
+         	<div class="col-sm-4">
+             
+             <input type="email" class="form-control" id="email" name="email" placeholder="Email" />
+            </div><!-- col -->
+          </div><!--row--> 
+          <div class="row">
+         	
+             <div class="col-sm-4">
+             <input type="textarea" rows="3" cols="10" class="form-control" id="email" name="email" placeholder="Message" />
+            </div><!-- col -->
+          </div><!--row--> 
+         
+          
+                   
+                     <div class="row">
+                    <div class="col-sm-3">
+                        <label for="Send"></label>
+                    </div><!-- col 3-->
+                    <div class="col-sm-3">
+               		 <button type="submit" class=" btn btn-success btn-lg">Submit</button>
+                   </div><!-- col 9-->
+                 </div><!-- row -->
+          
+                   
+          </form> 
+          </div><!--row-->
+    </div><!--container-->
+</div><!--wrap-->    
+          
+ 
+ </div><!-- col -->
+ <div class="col-sm-6">
+ <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3356.2154361903467!2d74.86300731518101!3d32.73346968098514!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x391e8487a84047e7%3A0xc3976f51e3542cb5!2sSuper+Bazar!5e0!3m2!1sen!2sin!4v1501611570200" width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
+ </div><!-- col -->
+ </div><!-- row -->
+<footer>
+<p class="text-center bgcolor"> Developed & Maintained By Manika Puri</p>
+</footer>
+
+
+
+
+
+
+
+
+
+
+
+<script src="js/jquery.min.js"></script>
+<script src="js/bootstrap.min.js"></script>
+</body>
+</html>
